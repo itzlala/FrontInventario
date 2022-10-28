b1647b324906c9365bad96a881aebfb027ff60e3
Miguel Arturo Jimenez May <miguel.jimenez@yucatan.gob.mx>
2022-10-28 16:42:34 -0500

    Login a medias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
--- src/app/inventario/login/login.component.ts
+++ src/app/inventario/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario.model';
 import { UsuarioService } from 'src/app/services/usuario.service';
+
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
